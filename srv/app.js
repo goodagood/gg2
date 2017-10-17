0a15f71969fdc8a240c2f11e0d7e408a0378309f
Ubuntu <ubuntu@ip-10-166-144-148.ap-northeast-1.compute.internal>
2017-10-17 01:36:54 +0000

    "sync"

--- srv/app.js
+++ srv/app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'node_modules'))); //indev
+app.use('/pic', express.static('/my/pic')); //indev
 
 
 const MongoSessionStore = require("connect-mongo")(session);
